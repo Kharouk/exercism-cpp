f95919dfa660ba5583ba954d86fd27d3cccdc0f0
Alex Kharouk <akharoukpp@gmail.com>
2019-08-10 22:47:43 +0100

    add hello world and leap

---
+++ hello-world/hello_world.cpp
@@ -0,0 +1,8 @@
+#include "hello_world.h"
+
+using std::string;
+
+string hello_world::hello() {
+    return "Hello, World!";
+}
+// test
---
+++ leap/leap.h
@@ -0,0 +1,12 @@
+//
+// Created by Alex Kharouk on 2019-08-10.
+//
+
+#ifndef LEAP_LEAP_H
+#define LEAP_LEAP_H
+
+namespace leap {
+    bool is_leap_year();
+}
+
+#endif //LEAP_LEAP_H
