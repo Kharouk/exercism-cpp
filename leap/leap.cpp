b16743bd3a3526a00c94e85c7abc56c210e0db8c
Alex Kharouk <akharoukpp@gmail.com>
2019-08-10 23:09:57 +0100

    refactor to one line

--- leap/leap.cpp
+++ leap/leap.cpp
@@ -2,8 +2,5 @@
 
 bool leap::is_leap_year(int year)
 {
-    if (year % 4 == 0) {
-        return year % 100 != 0 & year % 400 == 0;
-    }
-    return false;
+    return year % 4 == 0 & year % 100 != 0 & year % 400 == 0;
 }
